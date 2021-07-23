1c708947993bfc0600d8685d74aa8e9a7c6dc2f7
Alexey Vymyatnin <vymyatnin_av@lemz-t.ru>
2021-07-23 10:26:09 +0700

    Update webpack config Add mini-css-extract-plugin to dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 var path = require("path");
 
 module.exports = (env, argv) => {
-  console.log(env);
+  // console.log(env);
   return {
     entry: ["./src/index.tsx"],
     output: {
-      filename: "[name].[fullhash].js",
+      filename: "static/js/[name].[contenthash:8].js",
       path: path.resolve(__dirname, "build"),
+      chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
     },
     module: {
       rules: [
@@ -27,13 +29,25 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.(css|less)$/i,
-          use: ["style-loader", "css-loader", "less-loader"],
+          use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+        },
+        {
+          test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+          use: "url-loader",
         },
         {
           test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
           type: "asset/resource",
         },
       ],
+    },
+    stats: {
+      cached: false,
+      cachedAssets: false,
+      chunks: false,
+      chunkModules: false,
+      chunkOrigins: false,
+      modules: false,
     },
     resolve: {
       extensions: [".tsx", ".ts", ".js", ".jsx"],
@@ -49,16 +63,23 @@ module.exports = (env, argv) => {
       compress: true,
       port: 3000,
       hot: true,
+      stats: {
+        modules: false,
+      },
     },
     plugins: [
-      // ...
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({ template: "./public/index.html" }),
+      new MiniCssExtractPlugin({
+        filename: "static/css/[name].[contenthash:8].css",
+        chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+      }),
       new CopyWebpackPlugin({
         patterns: [
           {
             from: "src/assets",
             to: "assets",
+            noErrorOnMissing: true,
           },
         ],
       }),
