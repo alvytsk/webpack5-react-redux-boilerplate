bc1c2c73be4dfc5277ba79f7e7b7139014734e46
Alexey Vymyatnin <avymiatnin@aspirity.com>
2022-06-22 18:28:24 +0700

    Update packages to latest version. Update index.tsx to support React v18 features

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./state";
 
-ReactDOM.render(
+const main = (
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(main);
